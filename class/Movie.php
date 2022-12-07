d54be63bb18ae63a3c2703c2f9222a7fbecfdf20
andreaa21 <andrea.zollini@proton.me>
2022-12-07 16:16:38 +0100

    add movie.php

---
+++ class/Movie.php
@@ -0,0 +1,21 @@
+<?php
+
+class Movie{
+
+     public $title;
+     public $director;
+     public $release_year;
+
+     public function __construct($_title, $_director, $_release_year)
+     {
+        $this.title->$_title;
+        $this.director->$_director;
+        $this.release_year->$_release_year;
+     }
+
+}
+
+$OnceUpon = new Movie("Once upon a time in hollywood", "Quentin Tarantino", "2018");
+
+
+?>

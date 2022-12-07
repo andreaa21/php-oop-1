21939f4757691bc35e836b4369616fd42de4d2b4
andreaa21 <andrea.zollini@proton.me>
2022-12-07 18:19:06 +0100

    add layout

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $Wolf->setImage("https://i.pinimg.com/originals/4c/9b/8b/4c9b8bcdbfbca5a1bd7d8e9
 
 $MovieList = [$OnceUpon, $Wolf];
 
-var_dump($MovieList);
+// var_dump($MovieList);
 
 ?>
 
@@ -29,11 +29,30 @@ var_dump($MovieList);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="css/style.css">
     <title>PHPmovies</title>
 </head>
 
 <body>
 
+    <div class="container">
+        <div class="row p-5 text-center">
+
+            <?php foreach ($MovieList as $Movie) : ?>
+                <div class="col-3 movie-card d-flex flex-column m-3 p-3 text-center justify-content-between">
+                    <div class="card-image h-75">
+                        <img src="<?php echo $Movie->img ?>" alt="<?php echo $Movie->title ?>">
+                    </div>
+                    <div class="card-text h-25 d-flex flex-column justify-content-around pt-4">
+                        <span><?php echo $Movie->title ?></span>
+                        <span><?php echo $Movie->director ?></span>
+                        <span><?php echo $Movie->release_year ?></span>
+                    </div>
+                </div>
+            <?php endforeach ?>
+
+        </div>
+    </div>
 
 
 </body>

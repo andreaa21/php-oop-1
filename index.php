9ac7c346704f8d8f131acf37002774558179bec9
andreaa21 <andrea.zollini@proton.me>
2022-12-07 15:48:17 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+class Movie
+{
+    public $title;
+    public $director;
+    public $year_of_release;
+
+    public function __construct($_title, $_director, $_year_of_release)
+    {
+        $this . title->$_title;
+        $this . director->$_director;
+        $this . year_of_production->$_year_of_release;
+    }
+}
+
+$OnceUpon = new Movie("Once upon a time in Hollywood", "Quentin Tarantino", "2018");
+
+var_dump($OnceUpon);
